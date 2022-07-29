5773e955348fab89faa729f02233d48b1f2a83b1
YashiGoyal-02 <87610100+YashiGoyal-02@users.noreply.github.com>
2022-07-29 14:33:00 +0530

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,320 @@
+const questions = [
+    {
+        question: "Which of the following is not caused due to air pollution?",
+        optionA: "Heart Stroke",
+        optionB: "Lung Cancer",
+        optionC: "Asthama",
+        optionD: "Diabetes",
+        correctOption: "optionD",
+        explanation: "Breathing in air pollution can increase the risk of heart disease and stroke. It worsens asthma symptoms and can even cause lung cancer. However, diabetes is caused mainly due to bad lifestyle."
+    },
+
+    {
+        question: "Do you know eating plant-based food can help our planet, Want to know what it means?",
+        optionA: "Eating plants everyday",
+        optionB: "You can't eat after sunset",
+        optionC: "Eating food that comes from plants & not eating food that comes from animals",
+        optionD: "Eating only leaves and drinking water",
+        correctOption: "optionC",
+        explanation: "Calm down, you can eat everything you like and still save our planet. You can have all your cupcakes, chocolates, pizzas, butter chicken, everything you like. You just need to make a few changes in your diet and the raw material you use to cook that food. For eg, buy a vegan chocolate/ice cream instead of your regular milk chocolate & ice-creams, and use plant-based cheese for your favorite grilled cheese sandwich. Start by making small changes, and you can make an impact soon."
+    },
+
+    {
+        question: "Which of the following takes the longest time to biodegrade?",
+        optionA: "Wool Sock",
+        optionB: "Orange Peels",
+        optionC: "Nylon Fabric",
+        optionD: "Paper",
+        correctOption: "optionC",
+        explanation: "Nylon fabric takes about 30-40 years, Wool socks take about 1-5 years, Orange peels take about 6 months & paper takes 2-5 months to completely biodegrade when they are scattered as litters. Be mindful of how to dispose of your waste and where it goes."
+    },
+
+    {
+        question: "What are some common reuses of a plastic bag?",
+        optionA: "Liners for small trash cans",
+        optionB: "Keep in car for emergencies & clean up",
+        optionC: "For buying groceries and use them when packaging for travel",
+        optionD: "All of these",
+        correctOption: "optionD",
+        explanation: "Every time you have a plastic bag lying in your house, please do not straightway trash it, but try reusing it, to reduce the waste that lands in the landfills and later in the oceans negatively affecting our aquatic life."
+    },
+
+    {
+        question: "How do trees not help us?",
+        optionA: "Manufactures candies",
+        optionB: "Absorbs carbon dioxide",
+        optionC: "Provide fruits",
+        optionD: "Provides shelter",
+        correctOption: "optionA",
+        explanation: "Trees are vital. As the biggest plants on the planet, they give us oxygen, store carbon, stabilize the soil and give life to the world's wildlife. They also provide us with the materials for tools and shelter. But, sorry dear, for candies you need to go to a candy store. Plant trees for us, for our planet. Tie a #TreeAndHumanKnot"
+    },
+
+    {
+        question: "What is a part of the photosynthesis process?",
+        optionA: "Leaves absorb carbon dioxide from the air",
+        optionB: "Leaves absorb water from the soil through roots",
+        optionC: "Leaves use the energy of the sun to convert this into chemical compounds",
+        optionD: "All of these",
+        correctOption: "optionD",
+        explanation: "Photosynthesis is the process by which plants use sunlight, water, and carbon dioxide to create oxygen and energy in the form of sugar. And that’s how trees provide us oxygen and absorb carbon dioxide from the air."
+    },
+
+    {
+        question: "Which of the following is not a characteristic of living things?",
+        optionA: "Living beings never sleep",
+        optionB: "All livings beings reproduce",
+        optionC: "All livings beings need food",
+        optionD: "All living beings grow",
+        correctOption: "optionA", 
+        explanation: "Living things have a variety of characteristics that are displayed to different degrees: they respire, move, respond to stimuli, reproduce and grow, and are dependent on their environment. Plants, dogs, insects, human beings, etc everything is living things."
+    },
+
+    {
+        question: "The action of clearing a wide area of forests is known as deforestation. What are the effects of deforestation?",
+        optionA: "Increase in global warming",
+        optionB: "Causes soil erosion and floods",
+        optionC: "Loss of biodiversity, wildlife extinction & habitat loss",
+        optionD: "All of these",
+        correctOption: "optionD",
+        explanation: "Planting more trees than you chop down is the only way to make a significant change and save our planet. Deforestation is done for various reasons including overpopulation, desertification, forest fires, urbanization, etc. Plant more trees and save our planet."
+    },
+
+    {
+        question: "What causes plastic pollution?",
+        optionA: "It is non-biodegradable",
+        optionB: "Improper disposal",
+        optionC: "Plastics produce toxic chemicals and are made from fossil fuels",
+        optionD: "All of these",
+        correctOption: "optionD",
+        explanation: "Plastics are made from fossil fuels, finite and environmentally damaging resources. Single-use plastics like bottles, straws, and shopping bags are somehow the devils of all. So, what happens is, that single-use plastics are produced and used in mass, around the world, every day. This, hand in hand with improper disposal (non-recycling) and littering, produce plastic pollution on a global scale."
+    },
+
+    {
+        question: "How to reduce plastic pollution?",
+        optionA: "Use reusable water bottles",
+        optionB: "Avoid using disposable plastic plates or spoons for parties",
+        optionC: "Avoid using plastic party confetti",
+        optionD: "All of these",
+        correctOption: "optionD",
+        explanation: "Plastic pollution can be found on every beach in the world. 40% of plastic produced every year is single-use plastic. 10, 000 marine mammals and turtles are killed annually. Avoid using single-use plastics and always reuse, recycle, reduce the use of plastics and always dispose of them responsibly."
+    },
+
+    {
+        question: "Eating plant-based food can help us save our planet. Which of the following do you think is not allowed in a plant-based diet?",
+        optionA: "Butter & Cheese",
+        optionB: "Chocolate",
+        optionC: "Junk food",
+        optionD: "None of these",
+        correctOption: "optionD",
+        explanation: "You can have everything you like that is mentioned above. Veganism is not restrictive or boring, you just need to give yourself some time to understand & adapt it. In today’s world, where veganism is such a rage, everything you like has a vegan substitute. You name it & you have it. Vegan cheese, dairy-free milk, vegan chocolate & other desserts, pizza, noodles, pasta, Sev-puri, ice-creams, let’s just anything you wanna have. The mock meat industry is taking care of your non-veg taste buds too, you have ready-to-make/eat, vegan chicken-butter masala, keema curry & whatnot. If you don't believe, google -how to make vegan kheer, and you’ll have it."
+    },
+
+    {
+        question: "How can you help the environment?",
+        optionA: "Recycle as much as you can",
+        optionB: "Switch of electronics when not in use",
+        optionC: "Be creative to reduce waste",
+        optionD: "All of these",
+        correctOption: "optionD",
+        explanation: "You are the future generation, when it’s time for you to take the charge in your hands, you need to be prepared. Save our planet, save our lives. Adopt sustainable living because sustainability is here to stay or we may not."
+    },
+
+
+    {
+        question: "How can you help save the environment at school except in which of the following ways?",
+        optionA: "Think before you take a print-out",
+        optionB: "Use styrofoams for science projects",
+        optionC: "Avoid plastic lunch boxes and water bottles",
+        optionD: "Reduce food wastage",
+        correctOption: "optionB",
+        explanation: "Styrofoams are non-biodegradable and they release toxic chemicals into the environment. School provides the building block for your future, what you learn here, stays with you forever. Build good habits at the school level, to carry them forever. Save our planet, save the environment, plant more trees and follow the 3Rs: Reduce, Recycle, and Reuse. Tie a #TreeAndHumanKnot."
+    },
+
+    {
+        question: "Stationaries are absolute love for all the students. But, lesser do we realize, what happens after we thrash them or just stop using them, and where do they end up? How can you reduce stationery waste?",
+        optionA: "Use both sides of your notebook & scrapbook",
+        optionB: "Purchase stationeries made from recycled materials",
+        optionC: "Reuse old envelopes and notebook covers",
+        optionD: "All of these",
+        correctOption: "optionD", 
+        explanation: "As the future of the country, we need to take responsibility and learn about our surroundings and how we affect them. Stationeries are a crucial part of a student’s life and we can’t let them harm our environment. Let’s choose our favorite stationeries wisely and save our planet."
+    },
+
+    {
+        question: "Which of the following can be used in the process of composting?",
+        optionA: "Fruit Peels",
+        optionB: "Soda Cans",
+        optionC: "Juice Bottles",
+        optionD: "Aluminum foils",
+        correctOption: "optionA",
+        explanation: "Composting is the natural process of recycling organic matter, such as leaves and food scraps, into a valuable fertilizer that can enrich soil and plants. Soda cans, juice bottles, aluminum foils, chocolate wrappers all are non-biodegradable wastes and so we can’t use them in the process of composting. We use only biodegradable wastes for composting."
+    },
+
+    {
+        question: "Which of the following is not a non-biodegradable waste?",
+        optionA: "Batteries",
+        optionB: "Paper",
+        optionC: "Metal",
+        optionD: "Glass Pieces",
+        correctOption: "optionB",
+        explanation: "Paper industry causes deforestation, but still it’s biodegradable in nature. Non-biodegradable wastes are those, that cannot be decomposed or dissolved in natural agents, causing harm to the environment as they remain on earth for thousands of years without any degradation & fill the landfills and pollute the oceans. We should always dispose of the biodegradable and non-biodegradable wastes separately and responsibly."
+    },
+]
+
+
+let shuffledQuestions = [] //empty array to hold shuffled selected questions
+
+function handleQuestions() { 
+    //function to shuffle and push 10 questions to shuffledQuestions array
+    while (shuffledQuestions.length <= 9) {
+        const random = questions[Math.floor(Math.random() * questions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
+}
+
+
+let questionNumber = 1
+let playerScore = 0  
+let wrongAttempt = 0 
+let indexNumber = 0
+
+// function for displaying next question in the array to dom
+function NextQuestion(index) {
+    handleQuestions()
+    const currentQuestion = shuffledQuestions[index]
+    document.getElementById("question-number").innerHTML = questionNumber
+    document.getElementById("player-score").innerHTML = playerScore
+    document.getElementById("display-question").innerHTML = currentQuestion.question;
+    document.getElementById("option-one-label").innerHTML = currentQuestion.optionA;
+    document.getElementById("option-two-label").innerHTML = currentQuestion.optionB;
+    document.getElementById("option-three-label").innerHTML = currentQuestion.optionC;
+    document.getElementById("option-four-label").innerHTML = currentQuestion.optionD;
+
+}
+
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber] //gets current Question 
+    const currentQuestionAnswer = currentQuestion.correctOption //gets current Question's answer
+    const options = document.getElementsByName("option"); //gets all elements in dom with name of 'option' (in this the radio inputs)
+    let correctOption = null
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            //get's correct's radio input with correct answer
+            correctOption = option.labels[0].id
+        }
+    })
+   
+    //checking to make sure a radio input has been checked or an option being chosen
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked == false) {
+        document.getElementById('option-modal').style.display = "flex"
+    }
+
+    //checking if checked radio button is same as answer
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            playerScore++
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+
+        else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id
+            document.getElementById(wrongLabelId).style.backgroundColor = "red"
+            document.getElementById(correctOption).style.backgroundColor = "green"
+            wrongAttempt++
+            indexNumber++
+            //set to delay question number till when next question loads
+            setTimeout(() => {
+                questionNumber++
+            }, 1000)
+        }
+    })
+}
+
+
+
+//called when the next button is called
+function handleNextQuestion() {
+    checkForAnswer()
+    unCheckRadioButtons()
+    //delays next question displaying for a second
+    setTimeout(() => {
+        if (indexNumber <= 9) {
+            NextQuestion(indexNumber)
+        }
+        else {
+            handleEndGame()
+        }
+        resetOptionBackground()
+    }, 1000);
+}
+
+//sets options background back to null after display the right/wrong colors
+function resetOptionBackground() {
+    const options = document.getElementsByName("option");
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = ""
+    })
+}
+
+// unchecking all radio buttons for next question(can be done with map or foreach loop also)
+function unCheckRadioButtons() {
+    const options = document.getElementsByName("option");
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+// function for when all questions being answered
+function handleEndGame() {
+    let remark = null
+    let remarkColor = null
+
+    // condition check for player remark and remark color
+    if (playerScore <= 3) {
+        remark = "Bad Grades, Keep Practicing."
+        remarkColor = "red"
+    }
+    else if (playerScore >= 4 && playerScore < 7) {
+        remark = "Average Grades, You can do better."
+        remarkColor = "orange"
+    }
+    else if (playerScore >= 7) {
+        remark = "Excellent, Keep the good work going."
+        remarkColor = "green"
+    }
+    const playerGrade = (playerScore / 10) * 100
+
+    //data to display to score board
+    document.getElementById('remarks').innerHTML = remark
+    document.getElementById('remarks').style.color = remarkColor
+    document.getElementById('grade-percentage').innerHTML = playerGrade
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt
+    document.getElementById('right-answers').innerHTML = playerScore
+    document.getElementById('score-modal').style.display = "flex"
+
+}
+
+//closes score modal and resets game
+function closeScoreModal() {
+    questionNumber = 1
+    playerScore = 0
+    wrongAttempt = 0
+    indexNumber = 0
+    shuffledQuestions = []
+    NextQuestion(indexNumber)
+    document.getElementById('score-modal').style.display = "none"
+}
+
+//function to close warning modal
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = "none"
+}
